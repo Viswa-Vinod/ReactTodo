458276000963476a16a0769bca1ff60ef0c38e32
Vinod Kumar <vkviswa@gmail.com>
2017-07-22 13:57:12 +0530

    Add todos reducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -2,6 +2,8 @@ var expect = require('expect');
 var reducers = require('reducers');
 var df = require('deep-freeze-strict');
 
+
+
 describe('Reducers', ()=>{
 	describe('Search Text Reducer', ()=>{
 		it('should set search text',()=>{
@@ -23,6 +25,43 @@ describe('Reducers', ()=>{
 
 			var res = reducers.showCompletedReducer(df(false), df(action));
 			expect(res).toEqual(true);
+		});
+	});
+
+	describe('Todos Reducer', ()=>{
+		it('should add new todo',()=>{
+			var action = {
+				type: 'ADD_TODO',
+				text: 'Walk the dog'
+			};
+
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		it('should toggle todo',()=>{
+			var todo = [{
+				id: 1,
+				text: 'Walk the dog',
+				completed: false
+				
+			}, {
+				id: 2,
+				text: 'Walk the baby',
+				completed: false
+			}];
+
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			};
+
+			var res = reducers.todosReducer(df(todo),df(action));
+			
+			expect(res[0].completed).toEqual(true);
+			expect(res[0].completedAt).toNotBe(undefined);
 		});
 	});
 });
